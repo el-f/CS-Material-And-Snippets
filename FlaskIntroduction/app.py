4bdb5ecf555d4025e1dd7f8324ff681136d686f5
E. Fine <immortalisEL@gmail.com>
2020-09-22 05:05:07 +0300

    Removed Unused Import

--- FlaskIntroduction/app.py
+++ FlaskIntroduction/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
