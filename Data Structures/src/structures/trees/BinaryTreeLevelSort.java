62040d01baa029b1172df09037d33e49397a6a60
E. Fine <immortalisEL@gmail.com>
2020-10-01 18:23:47 +0300

    Added BinaryTreeLevelSort Kata Solution

---
+++ Data Structures/src/structures/trees/BinaryTreeLevelSort.java
@@ -0,0 +1,32 @@
+package structures.trees;
+
+import java.util.*;
+
+public class BinaryTreeLevelSort {
+
+    /*
+        This is my solution for this kata:
+        https://www.codewars.com/kata/52bef5e3588c56132c0003bc/train/java
+     */
+
+    public static List<Integer> treeByLevels(Node _node) {
+        List<Integer> result = new ArrayList<>();
+        if (_node == null) return result;
+
+        Queue<Node> nodes = new LinkedList<>();
+        nodes.add(_node);
+        while (!nodes.isEmpty()) {
+            Node current = nodes.remove();
+            result.add(current.value);
+            if (current.left != null) nodes.add(current.left);
+            if (current.right != null) nodes.add(current.right);
+        }
+
+        return result;
+    }
+}
+
+
+
+
+
