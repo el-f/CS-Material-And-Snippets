1f9e4b2c4a15f68760516aa8f1e18ed944f96c1c
E. Fine <immortalisEL@gmail.com>
2020-10-29 22:49:09 +0200

    Added Powersets recursive solution

---
+++ Data Structures/src/recursions/powersets.kt
@@ -0,0 +1,17 @@
+package recursions
+
+fun main() {
+    val array = listOf('a', 'b', 'c', 'd')
+    printSubgroups(array.size, array)
+}
+
+fun printSubgroups(j: Int, m: List<Char>) {
+    if (j <= 0) {
+        print('{')
+        m.forEach { print(if (it == m.last()) it else "$it, ") }
+        println('}')
+    } else {
+        printSubgroups(j - 1, m)
+        printSubgroups(j - 1, m - m[j - 1])
+    }
+}
