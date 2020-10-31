ca67d7b915ce0c230d5dc077160869b631430159
E. Fine <39451680+Elfein7Night@users.noreply.github.com>
2020-11-01 00:28:41 +0200

    Create tab.js

---
+++ Miscellaneous/tab.js
@@ -0,0 +1,19 @@
+/// twitch-videoad.js
+const origFetch = window.fetch;
+window.fetch = (url, init, ...args) => {
+	if (typeof url === "string") {
+		if (url.includes("/access_token")) {
+			url = url.replace("player_type=site", "player_type=embed");
+		} else if (
+			url.includes("/gql") &&
+			init &&
+			typeof init.body === "string" &&
+			init.body.includes("PlaybackAccessToken")
+		) {
+			const newBody = JSON.parse(init.body);
+			newBody.variables.playerType = "embed";
+			init.body = JSON.stringify(newBody);
+		}
+	}
+	return origFetch(url, init, ...args);
+};
