be475c4b1ed1feedcc6bbb17757ce532973d1eeb
E. Fine <immortalisEL@gmail.com>
2020-12-06 11:44:38 +0200

    Added JDBC_connect.java

---
+++ Learn SQL/JDBC_connect.java
@@ -0,0 +1,26 @@
+package src;
+
+import java.sql.*;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/demo?useUnicode"
+				+ "=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		String user = "user";
+		String password = "password";
+
+		try {
+			Connection connection = DriverManager.getConnection(url, user, password);
+			Statement myStatement = connection.createStatement();
+
+			ResultSet myResult = myStatement.executeQuery("select * from students");
+
+			while (myResult.next()) {
+				System.out.println(myResult.getInt("age") + " , " + myResult.getString("name"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
