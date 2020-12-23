982ae873c2ee918c031885763a65f4794ec5ca77
E. Fine <immortalisEL@gmail.com>
2020-12-23 21:06:32 +0200

    Added replacePairs.kt

---
+++ Data Structures/src/recursions/replacePairs.kt
@@ -0,0 +1,17 @@
+package recursions
+
+
+/*
+    replace every pair from right to left
+ */
+
+fun main() {
+    println(replaceWithLeft(123)) // 132
+    println(replaceWithLeft(1234)) // 2143
+    println(replaceWithLeft(12345)) // 13254
+}
+
+fun replaceWithLeft(n: Int): Int {
+    return if (n / 10 == 0) n
+    else n % 10 * 10 + n / 10 % 10 + replaceWithLeft(n / 100) * 100
+}
