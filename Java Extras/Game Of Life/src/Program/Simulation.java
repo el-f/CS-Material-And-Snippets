2dcda0a64c1b0d2943c20cade8130b734450f8d6
E. Fine <immortalisEL@gmail.com>
2020-12-27 00:10:05 +0200

    removed obsolete cli main

--- Java Extras/Game Of Life/src/Program/Simulation.java
+++ Java Extras/Game Of Life/src/Program/Simulation.java
@@ -12,20 +12,6 @@ public class Simulation {
         board = new int[width][height];
     }
 
-    @SuppressWarnings("all")
-    public void printBoard() {
-        for (int y = 0; y < height; y++) {
-            StringBuilder line = new StringBuilder();
-            for (int x = 0; x < width; x++) {
-                if (board[x][y] == 0)
-                    line.append("-");
-                else line.append("+");
-            }
-            System.out.println(line);
-        }
-        System.out.println();
-    }
-
     public void setAlive(int x, int y) {
         if (x < 0 || x >= width || y < 0 || y >= height)
             return;
@@ -69,15 +55,15 @@ public class Simulation {
             for (int x = 0; x < width; x++) {
                 int aliveNeighbours = countAliveNeighbours(x, y);
                 if (getCellValue(x, y) == 1) {
-                    if (aliveNeighbours < 2)
+                    if (aliveNeighbours < 2) {
                         newBoard[x][y] = 0;
-                    else if (aliveNeighbours == 2 || aliveNeighbours == 3)
+                    } else if (aliveNeighbours == 2 || aliveNeighbours == 3) {
                         newBoard[x][y] = 1;
-                    else
+                    } else {
                         newBoard[x][y] = 0;
-                } else {
-                    if (aliveNeighbours == 3)
-                        newBoard[x][y] = 1;
+                    }
+                } else if (aliveNeighbours == 3) {
+                    newBoard[x][y] = 1;
                 }
             }
         }
@@ -85,28 +71,7 @@ public class Simulation {
     }
 
     public void toggleState(int x, int y) {
-        if (getCellValue(x, y) == 0)
-            setAlive(x, y);
+        if (getCellValue(x, y) == 0) setAlive(x, y);
         else setDead(x, y);
-    }
-
-    // testing without gui
-    public static void main(String[] args) {
-        Simulation simulation = new Simulation(5, 5);
-        simulation.setAlive(0, 0);
-        simulation.setAlive(0, 1);
-        simulation.setAlive(1, 0);
-        simulation.setAlive(0, 2);
-        simulation.setAlive(0, 3);
-        simulation.setAlive(1, 1);
-        simulation.setAlive(1, 2);
-        simulation.setAlive(1, 3);
-        simulation.setAlive(2, 3);
-        simulation.printBoard();
-        simulation.step();
-        simulation.printBoard();
-        simulation.step();
-        simulation.printBoard();
-        System.out.println(simulation.countAliveNeighbours(3, 2));
     }
 }
