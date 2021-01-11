4c1ab226f19005665a7d35ff174516b0b84163eb
E. Fine <immortalisEL@gmail.com>
2021-01-11 11:50:23 +0200

    update UsefulFunctions.c

--- Learn C/UsefulFunctions.c
+++ Learn C/UsefulFunctions.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 int scanInt() {
     int num;
@@ -59,7 +60,7 @@ char *getStr(char *buf, size_t size) {
     return NULL;
 }
 
-#define MAX_LENGTH 256
+#define MAX_LENGTH 255
 
 char *getStrAuto() {
     char *theStr = NULL;
@@ -210,3 +211,169 @@ int isAllLetters(const char *str) {
     while ((c = *str) && (isalpha(c) || isblank(c))) ++str;
     return *str == '\0';
 }
+
+void generalArrayFunction(void *arr, int size, int typeSize, void(*func)(void *element)) {
+    for (int i = 0; i < size; i++)
+        func((char *) (arr) + i * typeSize);
+
+}
+
+char **splitCharsToWords(char *str, int *pCount, int *pTotalLength) {
+    char temp[255];
+    char *delimiters = " ";
+    char *word;
+    int count = 0;
+
+    strcpy(temp, str);
+    char **wordsArray = NULL;
+    *pTotalLength = 0;
+
+    word = strtok(temp, delimiters);
+    while (word != NULL) {
+        wordsArray = (char **) realloc(wordsArray, (count + 1) * sizeof(char *));
+        if (!wordsArray)
+            return 0;
+        wordsArray[count] = getDynStr(word);
+        count++;
+        *pTotalLength += (int) strlen(word);
+        word = strtok(NULL, delimiters);
+    }
+    *pCount = count;
+    return wordsArray;
+}
+
+char *myGets(char *buffer, int size, FILE *fp) {
+    if (buffer != NULL && size > 0) {
+        if (fgets(buffer, size, fp)) {
+            buffer[strcspn(buffer, "\n")] = '\0';
+            return buffer;
+        }
+        buffer[0] = '\0';
+    }
+    return NULL;
+}
+
+char *getStrAutoFromFile(FILE *fp) {
+    char *theStr = NULL;
+    int len;
+    char inpStr[MAX_LENGTH]; //var to hold str that user gives
+    myGets(inpStr, sizeof(inpStr), fp);
+
+    len = strlen(inpStr) + 1; // +1 for '\0'
+
+    theStr = malloc(len * sizeof(char)); //allocate mem
+    if (theStr != NULL) strcpy(theStr, inpStr); //fill mem
+
+    return theStr;
+}
+
+char *getDynStr(char *str) {
+    char *theStr;
+    theStr = (char *) malloc((strlen(str) + 1) * sizeof(char));
+    if (!theStr)
+        return NULL;
+
+    strcpy(theStr, str);
+    return theStr;
+}
+
+char *getStrExactName(const char *msg, FILE *fp) {
+    char *str;
+    char temp[MAX_LENGTH];
+    printf("%s\t", msg);
+    myGets(temp, MAX_LENGTH, fp);
+
+    str = getDynStr(temp);
+    return str;
+}
+
+int writeStringToFile(char *str, FILE *fp, const char *msg) {
+    int length = strlen(str);
+    length++;
+    if (!writeIntToFile(length, fp, msg))
+        return 0;
+
+    if (!writeCharsToFile(str, length, fp, msg))
+        return 0;
+
+
+    return 1;
+}
+
+int writeCharsToFile(char *arr, int size, FILE *fp, const char *msg) {
+    if (fwrite(arr, sizeof(char), size, fp) != size) {
+        puts(msg);
+        fclose(fp);
+        return 0;
+    }
+    return 1;
+
+}
+
+int writeIntToFile(int val, FILE *fp, const char *msg) {
+    if (fwrite(&val, sizeof(int), 1, fp) != 1) {
+        puts(msg);
+        fclose(fp);
+        return 0;
+    }
+    return 1;
+}
+
+char *readStringFromFile(FILE *fp, const char *msg) {
+    char *str;
+    int length;
+    if (!readIntFromFile(&length, fp, msg))
+        return NULL;
+    str = (char *) malloc((length) * sizeof(char));
+    if (!str) {
+        puts(msg);
+        fclose(fp);
+        return NULL;
+    }
+    if (fread(str, sizeof(char), length, fp) != length) {
+        puts(msg);
+        fclose(fp);
+        return NULL;
+    }
+    return str;
+
+}
+
+int readCharsFromFile(char *arr, int length, FILE *fp, const char *msg) {
+    if (fread(arr, sizeof(char), length, fp) != length) {
+        puts(msg);
+        fclose(fp);
+        return 0;
+    }
+    return 1;
+}
+
+int readIntFromFile(int *pVal, FILE *fp, const char *msg) {
+    if (fread(pVal, sizeof(int), 1, fp) != 1) {
+        puts(msg);
+        fclose(fp);
+        return 0;
+    }
+    return 1;
+
+}
+
+int isValidDate(int day, int month, int year) {
+    //check year
+    if (year >= 1900 && year <= 2100) {
+        //check month
+        if (month >= 1 && month <= 12) {
+            //check days
+            if (((day >= 1 && day <= 31) &&
+                 (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12))
+                || ((day >= 1 && day <= 30) && (month == 4 || month == 6 || month == 9 || month == 11))
+                || ((day >= 1 && day <= 28) && (month == 2))
+                || ((day == 29 && month == 2 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))))) {
+                return true;
+            } else printf("Invalid Day!\n");
+        } else printf("Invalid Month!\n");
+    } else printf("Invalid Year!\n");
+
+    return false;
+}
+
