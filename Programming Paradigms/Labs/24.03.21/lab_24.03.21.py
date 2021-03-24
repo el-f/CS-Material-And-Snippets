5b4c2a34463da6f5e23618281ae653dfd52eb510
E. Fine <immortalisEL@gmail.com>
2021-03-24 11:01:10 +0200

    shortened V2

--- Programming Paradigms/Labs/24.03.21/lab_24.03.21.py
+++ Programming Paradigms/Labs/24.03.21/lab_24.03.21.py
@@ -58,7 +58,7 @@ def twoLetterSubs(arr):
 
 
 def twoLetterSubs_V2(arr):
-    return [word[i] + word[i + 1] for word in arr for i in range(len(word) - 1)]
+    return [word[i:i + 2] for word in arr for i in range(len(word) - 1)]
 
 
 if __name__ == '__main__':
