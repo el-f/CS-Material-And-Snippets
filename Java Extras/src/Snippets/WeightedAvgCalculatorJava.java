9938645cbac9b69e6b39e2d1297fecca9d69483f
E. Fine <immortalisEL@gmail.com>
2021-05-16 07:29:25 +0300

    revert JDK 14 to 8

--- Java Extras/src/Snippets/WeightedAvgCalculatorJava.java
+++ Java Extras/src/Snippets/WeightedAvgCalculatorJava.java
@@ -5,7 +5,6 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
