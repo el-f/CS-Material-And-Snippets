9d19cd5a7b413086bf6958ba0f562243f7d865cb
E. Fine <immortalisEL@gmail.com>
2021-05-19 19:08:43 +0300

    removed prefix functionality

--- Learn Python/Snippets/terminal_emulator.py
+++ Learn Python/Snippets/terminal_emulator.py
@@ -4,10 +4,9 @@ import time
 import os
 from collections import deque
 from datetime import datetime
-from itertools import cycle
 
 is_windows = platform.system() == 'Windows'
-prefixes = cycle(["$ ", f"{os.getcwd()} $ ", ])
+ts = "$ "
 
 
 class TerminalEmulator:
@@ -20,7 +19,7 @@ class TerminalEmulator:
         self.history = []
         self.command_queue = deque()
         self.start_time = datetime.now()
-        self.prefix = next(prefixes)
+
         self.my_commands = {
             '!help': (lambda command: self.help(command), "show all commands or use !help [command]"),
             'cd': (lambda command: os.chdir(''.join(command.split()[1:])), "change directory"),
@@ -30,7 +29,6 @@ class TerminalEmulator:
                      "exit the terminal"),
             'mult': (lambda command: self.multiple_commands(command),
                      f"run multiple commands separated by '{self.MULTIPLE_COMMAND_SPLITTER}'"),
-            'prefix': (lambda _: setattr(self, 'prefix', next(prefixes)), "Cycle between the prefixes"),
         }
 
     def process_history(self, command: str):
@@ -75,7 +73,7 @@ class TerminalEmulator:
         print("############################")
         while True:
             if len(self.command_queue) == 0:
-                cmd = input(self.prefix)
+                cmd = input(ts)
                 self.command_queue.append(cmd)
             self.process_cmd()
 
