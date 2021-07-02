f36ccce4a23e753d476b2b62ecf306594969f118
E. Fine <immortalisEL@gmail.com>
2021-07-02 03:16:46 +0300

    Added comments support with '#' + nicer output

--- Learn Kotlin/src/snippets/WeightedAvgCalculator.kt
+++ Learn Kotlin/src/snippets/WeightedAvgCalculator.kt
@@ -4,14 +4,14 @@ import java.io.File
 import kotlin.math.max
 
 fun main() {
-    println("------------ All Marks ------------")
+    println("------------ All Marks ------------------")
     var wac = WeightedAvgCalculator("Learn Kotlin/src/snippets/myMarks")
     wac.printAll()
     wac.printAverage()
     wac.printAllRanges()
 
 
-    println("\n-------- technology Marks ---------")
+    println("\n-------- technology Marks ---------------")
     wac = WeightedAvgCalculator("Learn Kotlin/src/snippets/myTechnologyMarks")
     wac.printAll()
     wac.printAverage()
@@ -23,6 +23,9 @@ class WeightedAvgCalculator(
         private var marks: MutableList<Triple<String, Int, Double>> = ArrayList(),
         private var allNaz: Double = 0.0,
 ) {
+    private val firstPrintLineLen: Int = 25
+    private val secondPrintLineLen: Int = 9
+
     init {
         if (filePath != null)
             initListFromFile(filePath)
@@ -31,7 +34,7 @@ class WeightedAvgCalculator(
     private fun initListFromFile(filePath: String) {
         File(filePath)
                 .readLines()
-                .filter { it.isNotEmpty() }
+                .filter { it.isNotEmpty() && !it.startsWith("#") }
                 .map { it.split(Regex(",")) }
                 .forEach { marks.add(Triple(it[0].trim(), it[1].trim().toInt(), it[2].trim().toDouble())) }
         allNaz = marks.sumOf { it.third }
@@ -58,13 +61,13 @@ class WeightedAvgCalculator(
     }
 
     fun printAll() {
-        println("Course                %          NZ")
-        println("-----------------------------------")
+        println("Course                     %           NZ")
+        println("-----------------------------------------")
         marks.forEach {
             print(it.first + " ")
-            for (i in 0 until max(20, it.first.length + 1) - it.first.length) print("-")
+            for (i in 0 until max(firstPrintLineLen, it.first.length + 1) - it.first.length) print("-")
             print(" " + it.second + " ")
-            for (i in 0 until 8 - it.second.toString().length) print("-")
+            for (i in 0 until secondPrintLineLen - it.second.toString().length) print("-")
             println(" " + it.third)
         }
     }
