5e1a1f24e9468796cc21416394e2e15633e58ca8
E. Fine <immortalisEL@gmail.com>
2021-07-21 22:33:18 +0300

    made program flow much clearer

--- Java Extras/src/Threads/PlatformRunLater.java
+++ Java Extras/src/Threads/PlatformRunLater.java
@@ -3,6 +3,7 @@ package Threads;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -22,27 +23,53 @@ public class PlatformRunLater extends Application {
     public void start(Stage primaryStage) {
         label.setFont(Font.font(30));
         Button button = new Button("GO");
-        button.setOnAction(e -> {
+        VBox vbox = new VBox(50, button, label);
+        vbox.setAlignment(Pos.CENTER);
+        button.setOnAction(event -> {
             Thread t = new Thread(() -> {
                 try {
-
-                    Platform.runLater(() -> label.setText("working..."));
-                    Thread.sleep(5000);
+                    int countDown = 5;
+                    Platform.runLater(() -> label.setText("working:\t" + countDown));
+                    indicateProgress(label, countDown);
+                    Thread.sleep(countDown * 1000);
                     Platform.runLater(() -> label.setText("finished!"));
 
+                    addExitButton(vbox);
                 } catch (InterruptedException e1) {
                     // ignore
                 }
             });
             t.start();
-
         });
-        VBox vbox = new VBox(50, button, label);
-        vbox.setPadding(new Insets(50));
+
         Scene scene = new Scene(vbox, 300, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
 
+    }
+
+    @SuppressWarnings("all")
+    void indicateProgress(Label label, int countDown) {
+        new Thread(() -> {
+            for (char cd = Character.forDigit(countDown, 10); cd > '0'; cd--) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    // ignore
+                }
+                char finalCd = cd;
+                Platform.runLater(() -> label.setText(label.getText().replace(finalCd, (char) (finalCd - 1))));
+            }
+        }).start();
+    }
+
+    void addExitButton(VBox vbox) {
+        Button exitBtn = new Button("Exit");
+        exitBtn.setOnAction(event -> {
+            Platform.exit();
+            System.exit(0);
+        });
+        Platform.runLater(() -> vbox.getChildren().add(exitBtn));
     }
 
 }
