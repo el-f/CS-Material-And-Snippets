98ddfdfe6cc2c3c459e6879cb26f774e5978123a
E. Fine <immortalisEL@gmail.com>
2021-07-25 12:59:58 +0300

    Improved solution and added alternative.

--- Java Extras/src/Snippets/LCS.java
+++ Java Extras/src/Snippets/LCS.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+    Longest Common Sequence Solution
+ */
 public class LCS {
 
     @Data
@@ -18,7 +21,8 @@ public class LCS {
     static String lcs(String a, String b) {
         if (a.length() == 0 || b.length() == 0) return "";
 
-        if (cache.containsKey(new StringPair(a, b))) return cache.get(new StringPair(a, b));
+        StringPair current = new StringPair(a, b);
+        if (cache.containsKey(current)) return cache.get(current);
 
         String result;
         if (a.charAt(a.length() - 1) == b.charAt(b.length() - 1)) {
@@ -29,12 +33,34 @@ public class LCS {
             result = s1.length() > s2.length() ? s1 : s2;
         }
 
-        cache.put(new StringPair(a, b), result);
+        cache.put(current, result);
+        return result;
+    }
+
+    // same but traversing the string forward in stead of backwards
+    static String lcs2(String a, String b) {
+        if (a.length() == 0 || b.length() == 0) return "";
+
+        StringPair current = new StringPair(a, b);
+        if (cache.containsKey(current)) return cache.get(current);
+
+        String result;
+        if (a.charAt(0) == b.charAt(0)) {
+            result = a.charAt(0) + lcs2(a.substring(1), b.substring(1));
+        } else {
+            String s1 = lcs2(a.substring(1), b);
+            String s2 = lcs2(a, b.substring(1));
+            result = s1.length() > s2.length() ? s1 : s2;
+        }
+
+        cache.put(current, result);
         return result;
     }
 
     public static void main(String[] args) {
         System.out.println(lcs("nothardlythefinaltest", "zzzfinallyzzz").equals("final"));
         System.out.println(lcs("", "").equals(""));
+        System.out.println(lcs2("nothardlythefinaltest", "zzzfinallyzzz").equals("final"));
+        System.out.println(lcs2("", "").equals(""));
     }
 }
