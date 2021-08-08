70485fc53d7ba928b1e95e747b0426ab9ab533b7
E. Fine <immortalisEL@gmail.com>
2021-08-08 03:26:57 +0300

    Added most_significant_digit different way

--- Learn Python/Snippets/most_significant_digit.py
+++ Learn Python/Snippets/most_significant_digit.py
@@ -7,11 +7,20 @@ def most_significant_digit(n: int) -> int:
     return math.floor(math.pow(10, k))
 
 
+# same thing but we call the division explicitly 10^(a-b) = 10^a/10^b
+def most_significant_digit_2(n: int) -> int:
+    return n // 10 ** math.floor(math.log10(n))
+
+
 if __name__ == '__main__':
     print(
         all((
             most_significant_digit(354) == 3,
             most_significant_digit(123) == 1,
             most_significant_digit(987) == 9,
+
+            most_significant_digit_2(354) == 3,
+            most_significant_digit_2(123) == 1,
+            most_significant_digit_2(987) == 9,
         ))
     )
