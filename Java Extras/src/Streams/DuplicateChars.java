56c3d0af38e2a404095329ef40c6422b7faf719c
E. Fine <immortalisEL@gmail.com>
2021-08-16 00:09:37 +0300

    Added more Java Streams examples

---
+++ Java Extras/src/Streams/DuplicateChars.java
@@ -0,0 +1,38 @@
+package Streams;
+
+
+import java.util.*;
+
+import static java.lang.System.out;
+import static java.util.function.UnaryOperator.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+// solution for this kata: codewars.com/kata/54bf1c2cd5b56cc47f0007a1
+public class DuplicateChars {
+
+    static final boolean PRINT_MAP = false;
+
+    public static void main(String[] args) {
+        out.println(duplicateCount("hello world!"));
+        out.println(duplicateCount("the quick brown fox jumped over the lazy dog!"));
+        out.println(duplicateCount("abcdefghijklmnopqrstuvwxyz"));
+    }
+
+    private static Map<Character, Long> charFrequenciesMap(final String text) {
+        return text.codePoints()
+                .map(Character::toLowerCase)
+                .mapToObj(c -> (char) c)
+                .collect(groupingBy(identity(), counting()));
+    }
+
+    static long duplicateCount(final String text) {
+        if (PRINT_MAP) out.println(charFrequenciesMap(text));
+
+        return charFrequenciesMap(text)
+                .values().stream()
+                .filter(i -> i > 1)
+                .count();
+    }
+
+}
--- Java Extras/src/Streams/JavaStreams.java
+++ Java Extras/src/Streams/JavaStreams.java
@@ -3,97 +3,111 @@ package Streams;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.IntSummaryStatistics;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.lang.System.out;
+import static java.util.function.Function.identity;
 
 public class JavaStreams {
     static String path = Paths.get("").toAbsolutePath() + "/Java Extras/src/Streams";
 
     public static void main(String[] args) throws IOException {
+        int example = 0;
+
+        out.println("\n~~~~~~~ " + ++example);
         // 1. Integer Stream
         IntStream
                 .range(1, 10)
-                .forEach(System.out::print);
-        System.out.println();
+                .forEach(out::print);
+        out.println();
 
+        out.println("\n~~~~~~~ " + ++example);
         // 2. Integer Stream with skip
         IntStream
                 .range(1, 10)
                 .skip(5)
-                .forEach(System.out::println);
-        System.out.println();
+                .forEach(out::println);
+        out.println();
 
+        out.println("\n~~~~~~~ " + ++example);
         // 3. Integer Stream with sum
-        System.out.println(
+        out.println(
                 IntStream
                         .range(1, 5)
                         .sum());
-        System.out.println();
+        out.println();
 
+        out.println("\n~~~~~~~ " + ++example);
         // 4. Stream.of, sorted and findFirst
         Stream.of("Ava", "Aneri", "Alberto")
                 .sorted()
                 .findFirst()
-                .ifPresent(System.out::println);
+                .ifPresent(out::println);
 
+        out.println("\n~~~~~~~ " + ++example);
         // 5. Stream from Array, sort, filter and print
         String[] names = {"Al", "Ankit", "Kushal", "Brent", "Sarika", "amanda", "Hans", "Shivika", "Sarah"};
         Arrays.stream(names)    // same as Stream.of(names)
                 .filter(x -> x.startsWith("S"))
                 .sorted()
-                .forEach(System.out::println);
+                .forEach(out::println);
 
+        out.println("\n~~~~~~~ " + ++example);
         // 6. average of squares of an int array
         Arrays.stream(new int[]{2, 4, 6, 8, 10})
                 .map(x -> x * x)
                 .average()
-                .ifPresent(System.out::println);
+                .ifPresent(out::println);
 
+        out.println("\n~~~~~~~ " + ++example);
         // 7. Stream from List, filter and print
         List<String> people = Arrays.asList("Al", "Ankit", "Brent", "Sarika", "amanda", "Hans", "Shivika", "Sarah");
         people
                 .stream()
                 .map(String::toLowerCase)
                 .filter(x -> x.startsWith("a"))
-                .forEach(System.out::println);
+                .forEach(out::println);
 
+        out.println("\n~~~~~~~ " + ++example);
         // 8. Stream rows from text file, sort, filter, and print
         Stream<String> bands = Files.lines(Paths.get(path + "/bands.txt"));
         bands
                 .sorted()
                 .filter(x -> x.length() > 13)
-                .forEach(System.out::println);
+                .forEach(out::println);
         bands.close();
 
+        out.println("\n~~~~~~~ " + ++example);
         // 9. Stream rows from text file and save to List
         List<String> bands2 = Files.lines(Paths.get(path + "/bands.txt"))
                 .filter(x -> x.contains("jit"))
                 .collect(Collectors.toList());
-        bands2.forEach(System.out::println);
+        bands2.forEach(out::println);
 
+        out.println("\n~~~~~~~ " + ++example);
         // 10. Stream rows from CSV file and count
         Stream<String> rows1 = Files.lines(Paths.get(path + "/data.txt"));
         int rowCount = (int) rows1
                 .map(x -> x.split(","))
                 .filter(x -> x.length == 3)
                 .count();
-        System.out.println(rowCount + " rows.");
+        out.println(rowCount + " rows.");
         rows1.close();
 
+        out.println("\n~~~~~~~ " + ++example);
         // 11. Stream rows from CSV file, parse data from rows
         Stream<String> rows2 = Files.lines(Paths.get(path + "/data.txt"));
         rows2
                 .map(x -> x.split(","))
                 .filter(x -> x.length == 3)
                 .filter(x -> Integer.parseInt(x[1]) > 15)
-                .forEach(x -> System.out.println(x[0] + "  " + x[1] + "  " + x[2]));
+                .forEach(x -> out.println(x[0] + "  " + x[1] + "  " + x[2]));
         rows2.close();
 
+        out.println("\n~~~~~~~ " + ++example);
         // 12. Stream rows from CSV file, store fields in HashMap
         Stream<String> rows3 = Files.lines(Paths.get(path + "/data.txt"));
         Map<String, Integer> map = rows3
@@ -105,19 +119,50 @@ public class JavaStreams {
                         x -> Integer.parseInt(x[1])));
         rows3.close();
         for (String key : map.keySet()) {
-            System.out.println(key + "  " + map.get(key));
+            out.println(key + "  " + map.get(key));
         }
 
+        out.println("\n~~~~~~~ " + ++example);
         // 13. Reduction - sum
         double total = Stream.of(7.3, 1.5, 4.8, 5.1)
                 .reduce(0.0, (Double a, Double b) -> a / 3 == 0.5 ? a : b);
 //                .reduce(0.0, Double::sum);
 //                .reduce(0.0, (Double a, Double b) -> a + b);
-        System.out.println("Total = " + total);
+        out.println("Total = " + total);
 
+        out.println("\n~~~~~~~ " + ++example);
         // 14. Reduction - summary statistics
         IntSummaryStatistics summary = IntStream.of(7, 2, 19, 88, 73, 4, 10)
                 .summaryStatistics();
-        System.out.println(summary);
+        out.println(summary);
+
+        List<String> words = Arrays.stream("The quick brown fox jumped over the lazy dog".split("\\s+"))
+                .collect(Collectors.toList());
+
+        out.println("\n~~~~~~~ " + ++example);
+        // 15. toMap(..)
+        Map<String, Integer> lengths = words.stream()
+                .collect(Collectors.toMap(identity(), String::length));
+        out.println(lengths);
+
+        out.println("\n~~~~~~~ " + ++example);
+        // 16. toSet(..)
+        Set<Integer> unique = Stream.of(5, 1, 2, 3, 4, 5, 4, 3, 2, 1)
+                .collect(Collectors.toSet());
+        out.println(unique);
+
+        out.println("\n~~~~~~~ " + ++example);
+        // 17. flatMap(..)
+        List<Character> chars = words.stream()
+                .flatMap(w -> w.chars().mapToObj(c -> (char) c))
+                .collect(Collectors.toList());
+        out.println(chars);
+
+
+        out.println("\n~~~~~~~ " + ++example);
+        // 18. groupingBy(..)
+        Map<Integer, List<String>> wordsByLength = words.stream()
+                .collect(Collectors.groupingBy(String::length));
+        System.out.println(wordsByLength);
     }
 }
