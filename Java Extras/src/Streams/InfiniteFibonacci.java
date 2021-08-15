8b3e0f4441dd498c6bbac3abf73999582df4d565
E. Fine <immortalisEL@gmail.com>
2021-08-15 09:18:14 +0300

    long -> BigInteger

--- Java Extras/src/Streams/InfiniteFibonacci.java
+++ Java Extras/src/Streams/InfiniteFibonacci.java
@@ -1,15 +1,22 @@
 package Streams;
 
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.LongStream;
+import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 
 public class InfiniteFibonacci {
 
-    // my solution for this kata: 55695bc4f75bbaea5100016b
+    // my solution for this kata: 55695bc4f75bbaea5100016b (a bit enhanced for accuray)
 
-    public static LongStream generateFibonacciSequence() {
-        AtomicLong lastHolder = new AtomicLong(1);
-        return LongStream.iterate(1, lastHolder::getAndAdd);
+    public static Stream<BigInteger> generateFibonacciSequence() {
+        AtomicReference<BigInteger> lastHolder = new AtomicReference<>(BigInteger.valueOf(1));
+        return Stream.iterate(BigInteger.ONE, x -> x.add(lastHolder.getAndSet(x)));
+    }
+
+    public static void main(String[] args) {
+        generateFibonacciSequence()
+                .limit(1000)
+                .forEach(System.out::println);
     }
 
 }
