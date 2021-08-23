4b6976c0f53e7a7e075fe40b9aee370d1b628d23
E. Fine <immortalisEL@gmail.com>
2021-08-23 15:42:16 +0300

    Added TwoSum.java

---
+++ Java Extras/src/Snippets/TwoSum.java
@@ -0,0 +1,25 @@
+package Snippets;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+// my solution for this kata: 52c31f8e6605bcc646000082
+public class TwoSum {
+    public static int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int remaining;
+        for (int i = 0; i < numbers.length; i++) {
+            remaining = target - numbers[i];
+            if (map.containsKey(remaining)) {
+                return new int[]{i, map.get(remaining)};
+            }
+            map.put(numbers[i], i);
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println((Arrays.toString(twoSum(new int[]{1, 2, 3, 5, 9, 8, 5, 4, 11, 13}, 17))));
+    }
+}
