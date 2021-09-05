08408f75228fc953263c5f642144f8fdb7542caa
E. Fine <immortalisEL@gmail.com>
2021-09-05 10:22:44 +0300

    Better output + misc improvements.

--- Java Extras/src/Streams/InfinitePrimes.java
+++ Java Extras/src/Streams/InfinitePrimes.java
@@ -1,6 +1,6 @@
 package Streams;
 
-import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 import java.util.BitSet;
 
@@ -9,32 +9,31 @@ public class InfinitePrimes {
 
     static BitSet sieve;
 
-    static boolean first = true;
+    static {
+        int window = (int) 1E9;
+        sieve = new BitSet(window);
+        sieve.flip(2, window - 1);
 
-    public static IntStream stream() {
-        if (first) {
-            int window = (int) 1E9;
-            sieve = new BitSet(window);
-            sieve.flip(2, window - 1);
-
-            for (int i = sieve.nextSetBit(2); 0 < i && i < 31622; i = sieve.nextSetBit(i + 1)) {
-                for (int j = 2 * i; j < window; j += i) {
-                    sieve.clear(j);
-                }
+        for (int i = sieve.nextSetBit(2); 0 < i && i < 31622; i = sieve.nextSetBit(i + 1)) {
+            for (int j = 2 * i; j < window; j += i) {
+                sieve.clear(j);
             }
-            first = false;
         }
+    }
 
+    public static IntStream streamPrimes() {
         return IntStream.iterate(2, i -> sieve.nextSetBit(i + 1));
     }
 
     public static void main(String[] args) {
-        int printEvery = (int) 1E6;
-        Iterator<Integer> it = stream().iterator();
-        for (int i = 0; i < 1E9; i++) {
-            if (i % printEvery == 0) {
-                System.out.println(it.next());
-            } else it.next();
-        }
+        AtomicInteger printIndex = new AtomicInteger(0);
+        System.out.println(sieve.cardinality() + " primes in range of 0 - 1E9");
+        System.out.println("10 samples of primes in that range:");
+        streamPrimes().limit(sieve.cardinality())
+                .forEach(x -> {
+                    if (printIndex.getAndIncrement() % 5E6 == 0){
+                        System.out.println(x);
+                    }
+                });
     }
 }
