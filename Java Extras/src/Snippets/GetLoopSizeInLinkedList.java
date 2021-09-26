8297782011c60b40a4314901a5517144f17b62b5
E. Fine <immortalisEL@gmail.com>
2021-09-27 00:27:54 +0300

    Made solution safer.

--- Java Extras/src/Snippets/GetLoopSizeInLinkedList.java
+++ Java Extras/src/Snippets/GetLoopSizeInLinkedList.java
@@ -3,26 +3,23 @@ package Snippets;
 import java.util.HashMap;
 
 // my solution for this kata: 52a89c2ea8ddc5547a000863
-
 public class GetLoopSizeInLinkedList<V> {
 
-    HashMap<Node<V>, Integer> map = new HashMap<>();
-
     public int loopSize(Node<V> node) {
+        HashMap<Node<V>, Integer> map = new HashMap<>();
         int iter = 0;
 
-        while (true) {
+        while (node != null) {
             if (map.containsKey(node)) return iter - map.get(node);
             map.put(node, iter);
             iter++;
             node = node.getNext();
         }
-
+        return -1;
     }
 }
 
 abstract class Node<T> {
     abstract Node<T> getNext();
-
     abstract T getValue();
 }
