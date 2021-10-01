5cbdb9ad8f5f1883643b0ea9d84a9d3f0fd6a790
E. Fine <immortalisEL@gmail.com>
2021-10-01 20:55:05 +0300

    Added javassist

--- Java Extras/src/Snippets/C22.java
+++ Java Extras/src/Snippets/C22.java
@@ -1,13 +1,21 @@
 package Snippets;
 
 //import net.bytebuddy.agent.ByteBuddyAgent;
-
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 import net.bytebuddy.matcher.ElementMatchers;
 import net.bytebuddy.implementation.*;
 
+import javassist.*; // for the alternative solution
+
+// need to move to different file to make the solutions work
+abstract class Boris {
+    @SuppressWarnings("all")
+    public final boolean isInsane() {
+        return false;
+    }
+}
 
 /*
     my solution for this kata: 5510caecaacf801f820002ac
@@ -15,9 +23,12 @@ import net.bytebuddy.implementation.*;
  */
 class C22 extends Boris {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Boris boris2 = C22_alternative.trick();
+        if (!boris2.isInsane()) fail("boris2 is not insane!");
         Boris boris = C22.trick();
-        if (!boris.isInsane()) fail("Keep fighting!");
+        if (!boris.isInsane()) fail("boris is not insane!");
+
     }
 
     @SuppressWarnings("all")
@@ -48,8 +59,21 @@ class C22 extends Boris {
     }
 }
 
-abstract class Boris {
-    public final boolean isInsane() {
-        return false;
+// alternative solution
+class C22_alternative {
+    public static Boris trick() throws Exception {
+        CtClass borisClass = ClassPool.getDefault().get("Boris");
+        // remove abstract modifier from the class in order to make it possible to create an instance of this class
+        borisClass.setModifiers(Modifier.PUBLIC);
+        CtMethod m = borisClass.getDeclaredMethod("isInsane");
+        // remove unwanted method to replace it with desired one
+        borisClass.removeMethod(m);
+        // creating a method with desired behavior
+        CtMethod om = CtNewMethod.make("public boolean isInsane() { return true; }", borisClass);
+        borisClass.addMethod(om);
+        return (Boris) borisClass.toClass().getConstructor().newInstance();
     }
 }
+
+
+
