969a2ac33049f9cb6637b0c6d624efd5fe248201
Elazar Fine <39451680+Elfein7Night@users.noreply.github.com>
2021-11-10 14:38:24 +0200

    Update kmp.py

--- Algorithms/kmp.py
+++ Algorithms/kmp.py
@@ -1,43 +1,37 @@
 class KMP:
-    class OneIndexedString:
-        def __init__(self, s):
-            self.s = s
-
-        def __getitem__(self, i):
-            return self.s[i - 1]
-
     def __init__(self, pattern):
-        self.pattern = self.OneIndexedString(pattern)
+        self.pattern = pattern
         self.m = len(pattern)
         self.states = [0] * (self.m + 1)
         self.set_states()
 
     def set_states(self):
         for i in range(1, self.m):
-            self.states[i + 1] = self.delta(self.states[i], self.pattern[i + 1])
+            self.states[i + 1] = self.delta(self.states[i], self.pattern[i])
 
     def delta(self, state_i, c):
-        if c == self.pattern[state_i + 1]:
+        if c == self.pattern[state_i]:
             return state_i + 1
         if state_i == 0:
             return 0
         return self.delta(self.states[state_i], c)
 
     def search(self, text):
-        t = self.OneIndexedString(text)
         positions_found = []
         state = 0
-        for i in range(1, len(text) + 1):
-            state = self.delta(state, t[i])
+        for i in range(0, len(text)):
+            state = self.delta(state, text[i])
             if state == self.m:
                 state = self.states[self.m]
-                positions_found.append(i - self.m)
-
+                positions_found.append(i - self.m + 1)
         return positions_found
 
 
 if __name__ == '__main__':
-    kmp = KMP("abc")
-    print(kmp.search("abcdabcde"))
-    print(kmp.search("abcabcabcabc"))
+    search_abc = KMP("abc")
+    print(search_abc.search("abcdabcde"))
+    print(search_abc.search("abcabcabcabc"))
 
+    search_ababc = KMP("abab")
+    print(search_ababc.search("abababab"))
+    print(search_ababc.search("aaababaabab"))
