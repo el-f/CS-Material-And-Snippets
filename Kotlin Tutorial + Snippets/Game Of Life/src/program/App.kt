693af25d3f12ffe93e374ccf477ca7321af19580
E. Fine <immortalisEL@gmail.com>
2021-11-14 06:01:48 +0200

    Added onCloseRequest

--- Kotlin Tutorial + Snippets/Game Of Life/src/program/App.kt
+++ Kotlin Tutorial + Snippets/Game Of Life/src/program/App.kt
@@ -4,13 +4,14 @@ import javafx.stage.Stage
 import javafx.scene.Scene
 import com.sun.javafx.application.LauncherImpl
 import javafx.application.Application
+import kotlin.system.exitProcess
 
 class App : Application() {
     override fun start(primaryStage: Stage) {
         val mainView = MainView()
-        val scene = Scene(mainView, 950.0, 855.0)
         primaryStage.title = "Game of Life"
-        primaryStage.scene = scene
+        primaryStage.scene = Scene(mainView, 950.0, 855.0)
+        primaryStage.setOnCloseRequest { exitProcess(0) }
         primaryStage.show()
         mainView.draw()
     }
