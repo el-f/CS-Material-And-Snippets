ab2fd1304bfe95ff1baf4041881ffa5bf71378ef
E. Fine <immortalisEL@gmail.com>
2021-11-14 05:47:44 +0200

    Translated main to Kotlin

--- Kotlin Tutorial + Snippets/Game Of Life/src/program/App.kt
+++ Kotlin Tutorial + Snippets/Game Of Life/src/program/App.kt
@@ -1,28 +1,21 @@
-package program;
+package program
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.stage.Stage
+import javafx.scene.Scene
+import com.sun.javafx.application.LauncherImpl
+import javafx.application.Application
 
-public class App extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setOnCloseRequest(r -> {
-            Platform.exit();
-            System.exit(0);
-        });
-
-        MainView mainView = new MainView();
-        Scene scene = new Scene(mainView, 950, 855);
-        primaryStage.setTitle("Game of Life");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        mainView.draw();
+class App : Application() {
+    override fun start(primaryStage: Stage) {
+        val mainView = MainView()
+        val scene = Scene(mainView, 950.0, 855.0)
+        primaryStage.title = "Game of Life"
+        primaryStage.scene = scene
+        primaryStage.show()
+        mainView.draw()
     }
+}
 
-    public static void main(String[] args) {
-        launch();
-    }
+fun main(args: Array<String>) {
+    LauncherImpl.launchApplication(App::class.java, args)
 }
