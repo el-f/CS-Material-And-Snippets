98a640275cddf8e3414d131c914e925d1deb355b
E. Fine <immortalisEL@gmail.com>
2022-02-23 18:11:21 +0200

    Added PC lab2 files

---
+++ Parallel Computation/labs/lab 2/MPI_Probe_example.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <math.h>
+
+int main(int argc, char **argv) {
+    int myid, numprocs;
+    MPI_Status status;
+    int count;
+ 
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    
+   
+    if (myid == 0) {
+		int data[3] = {200, 300, 400};
+    	MPI_Send(data, 3, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    } else {
+		int *msg, j;
+		MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
+
+		MPI_Get_count(&status, MPI_INT, &count);
+		printf("getting count = %d\n",count);
+
+    	msg = (int*) malloc(count*sizeof(int));
+    	
+		MPI_Recv(msg, count,MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+    	
+    	for(j = 0; j < count; j++) {
+			printf("%d ",msg[j]);
+    	}
+		
+		printf("\n");
+    }
+	
+    MPI_Finalize();
+}
---
+++ Parallel Computation/labs/lab 2/sendRecv.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "mpi.h"
+
+int main(int argc, char* argv[]){
+	int x, y;
+	int my_rank; 			/* rank of process */
+	int p;       			/* number of processes */
+	MPI_Status status ;   	/* return status for receive */
+	
+	/* start up MPI */	
+	MPI_Init(&argc, &argv);
+	
+	/* find out process rank */
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
+	
+	/* find out number of processes */
+	MPI_Comm_size(MPI_COMM_WORLD, &p); 
+	
+	if (p != 2) { // Handle invalid input p
+		printf("Error! p=%d, please run with p=2\n", p);
+		MPI_Abort(MPI_COMM_WORLD, __LINE__); // __LINE__ = current line number
+	}
+	
+	if (my_rank == 0) {
+		x = 7;
+		MPI_Send(&x, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+		MPI_Recv(&x, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
+		printf("The new value of x = %d\n", x);
+	} else {
+		MPI_Recv(&y, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		y *= 3;
+		MPI_Send(&y, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
+	
+	/* shut down MPI */
+	MPI_Finalize(); 
+	
+	
+	return 0;
+}
