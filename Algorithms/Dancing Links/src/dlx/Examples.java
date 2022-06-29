81f1e9d2f3ea2580f531257b56838a842a7cd885
E. Fine <immortalisEL@gmail.com>
2022-06-29 07:34:08 +0300

    Added dancing links algorithm

---
+++ Algorithms/Dancing Links/src/dlx/Examples.java
@@ -0,0 +1,144 @@
+package dlx;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import java.util.*;
+
+public class Examples {
+
+    static void runCoverExample() {
+        int[][] example = {
+                { 0, 0, 1, 0, 1, 1, 0 },
+                { 1, 0, 0, 1, 0, 0, 1 },
+                { 0, 1, 1, 0, 0, 1, 0 },
+                { 1, 0, 0, 1, 0, 0, 0 },
+                { 0, 1, 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 1, 1, 0, 1 }
+        };
+
+        DancingLinks DLX = new DancingLinks(example);
+        DLX.runSolver();
+    }
+
+    private static int[][] fromString(String s) {
+        int[][] board = new int[9][9];
+        for (int i = 0; i < 81; i++) {
+            char c = s.charAt(i);
+            int row = i / 9;
+            int col = i % 9;
+            if (c != '.') {
+                board[row][col] = c - '0';
+            }
+        }
+        return board;
+    }
+
+
+    private static void runExample() {
+
+        String[] diffs = { "simple.txt", "easy.txt", "intermediate.txt", "expert.txt" };
+
+
+        BufferedReader reader;
+        String text;
+
+        for (String diff : diffs) {
+
+            String filename = "boards/" + diff;
+
+            List<Long> timings = new ArrayList<>();
+
+            try {
+                reader = new BufferedReader(new FileReader(filename));
+
+                while ((text = reader.readLine()) != null) {
+
+                    int[][] sudoku = fromString(text);
+
+                    // change below to whichever one you want to try out
+                    SudokuDLX solver = new SudokuDLX();
+//                    NaiveSudokuSolver solver = new NaiveSudokuSolver();
+
+                    long milis = System.nanoTime();
+
+                    solver.solve(sudoku);
+
+                    long elapsed = System.nanoTime() - milis;
+
+                    timings.add(elapsed);
+
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("STATS: " + diff + "\n");
+            printStats(timings);
+
+        }
+    }
+
+    private static void printStats(List<Long> timings) {
+        long min = timings.get(0);
+        long max = timings.get(0);
+
+        long sum = 0;
+        long sqsum = 0;
+
+        for (long ll : timings) {
+            min = Math.min(min, ll);
+            max = Math.max(max, ll);
+            sum += ll;
+        }
+
+        double avg = sum / timings.size();
+
+        for (long ll : timings) {
+            sqsum += (ll - avg) * (ll - avg);
+        }
+
+        double std = Math.sqrt(sqsum / timings.size());
+
+
+        System.out.println("min: " + min * 1e-6);
+        System.out.println("max: " + max * 1e-6);
+        System.out.println("avg: " + avg * 1e-6);
+        System.out.println("std: " + std * 1e-6);
+    }
+
+    static void runSudokuExample() {
+        String[] exampleSudoku = { "..9748...", "7........", ".2.1.9...",
+                "..7...24.", ".64.1.59.", ".98...3..", "...8.3.2.",
+                "........6", "...2759.." };
+
+        int[][] hardest = {
+                { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
+                { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+                { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+                { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+                { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
+                { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+                { 0, 9, 0, 0, 0, 0, 4, 0, 0 }
+        }; // apparently the hardest sudoku
+
+//        
+//        SudokuDLX sudoku = new SudokuDLX();
+//        sudoku.solve(hardest);
+
+        NaiveSudokuSolver naive = new NaiveSudokuSolver();
+        naive.solve(hardest);
+    }
+
+    public static void main(String[] args) {
+//        runCoverExample();
+//        runSudokuExample();
+
+        runExample();
+    }
+
+}
---
+++ Algorithms/Dancing Links/src/dlx/SolutionHandler.java
@@ -0,0 +1,63 @@
+package dlx;
+
+import dlx.DancingLinks.*;
+
+import java.util.*;
+
+import dlx.SudokuDLX.*;
+
+public interface SolutionHandler {
+    void handleSolution(List<DancingNode> solution);
+}
+
+class SudokuHandler implements SolutionHandler {
+    int size = 9;
+
+    public void handleSolution(List<DancingNode> answer) {
+        int[][] result = parseBoard(answer);
+        AbstractSudokuSolver.printSolution(result);
+    }
+
+    private int[][] parseBoard(List<DancingNode> answer) {
+        int[][] result = new int[size][size];
+        for (DancingNode n : answer) {
+            DancingNode rcNode = n;
+            int min = Integer.parseInt(rcNode.C.name);
+            for (DancingNode tmp = n.R; tmp != n; tmp = tmp.R) {
+                int val = Integer.parseInt(tmp.C.name);
+                if (val < min) {
+                    min = val;
+                    rcNode = tmp;
+                }
+            }
+            int ans1 = Integer.parseInt(rcNode.C.name);
+            int ans2 = Integer.parseInt(rcNode.R.C.name);
+            int r = ans1 / size;
+            int c = ans1 % size;
+            int num = (ans2 % size) + 1;
+            result[r][c] = num;
+        }
+        return result;
+    }
+
+
+    public SudokuHandler(int boardSize) {
+        size = boardSize;
+    }
+
+}
+
+class DefaultHandler implements SolutionHandler {
+    public void handleSolution(List<DancingNode> answer) {
+        for (DancingNode n : answer) {
+            String ret = "";
+            ret += n.C.name + " ";
+            DancingNode tmp = n.R;
+            while (tmp != n) {
+                ret += tmp.C.name + " ";
+                tmp = tmp.R;
+            }
+            System.out.println(ret);
+        }
+    }
+}
