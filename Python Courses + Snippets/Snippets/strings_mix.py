d2c877598c1a9d90499c701d6e7abbdc9d28eb6d
Elazar Fine <39451680+Elfein7Night@users.noreply.github.com>
2022-07-25 10:19:51 +0300

    Update strings_mix.py

--- Python Courses + Snippets/Snippets/strings_mix.py
+++ Python Courses + Snippets/Snippets/strings_mix.py
@@ -34,6 +34,6 @@ if __name__ == '__main__':
     assert mix(" In many languages", " there's a pair of functions") == \
            "1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt"
     assert mix("Lords of the Fallen", "gamekult") == "1:ee/1:ll/1:oo"
-    assert mix("codewars", "codewars") == ""
+    assert mix("a string", "a string") == ""
     assert mix("A generation must confront the looming ",
                "codewarrs") == "1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr"
