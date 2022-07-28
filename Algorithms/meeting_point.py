3e67088509e4b1779e7353d52d5b66d1f21ec9b5
Elazar Fine <39451680+Elfein7Night@users.noreply.github.com>
2022-07-28 10:27:57 +0300

    Update meeting_point.py

--- Algorithms/meeting_point.py
+++ Algorithms/meeting_point.py
@@ -29,3 +29,50 @@ def converge(g, us):
                     tmp.append(neigh)
             else:
                 floods[i][0] = tmp
+
+# #################################
+from heapq import heappush, heappop
+
+def converge_v2(g, coins):
+    coins = set(coins)
+    if len(coins) <= 1: return 0
+    # keeps track of which coins has reached each vertex in an even or odd number of steps
+    reached = {vertex: {0: 0, 1: 0} for vertex in g}
+    heap = []
+    p = 1
+    for coin in coins:
+        reached[coin][0] = p
+        heappush(heap, (0, coin))
+        p <<= 1
+    p -= 1
+
+    while heap:
+        step, vertex = heappop(heap)
+        parity = step % 2
+        reached_coins = reached[vertex][parity]
+        for v in g[vertex]:
+            if reached[v][1 - parity] | reached_coins != reached[v][1 - parity]:
+                reached[v][1 - parity] |= reached_coins
+                if reached[v][1 - parity] == p: return step + 1
+                heappush(heap, (step + 1, v))
+                
+# #################################               
+from itertools import count, cycle
+
+def converge_v3(g, cn):
+    q = {x: 0 for x in g}
+    r = {**q}
+    h = iter(cycle(((q,1),(r,2))))
+    m = {x:[{x},set(),set()] for x in cn}
+    ab = list(map(lambda x: [{x}, set(), set()], cn))
+    for o in count():
+        d, e = next(h)
+        for i, x in enumerate(ab):
+            a, b, c = x[0], x[e], set()
+            if not a: return None
+            for v in a - b:
+                d[v] += 1
+                if d[v] == len(cn): return o
+                b.add(v)
+                c.update(g[v])
+            else: ab[i][0] = c
